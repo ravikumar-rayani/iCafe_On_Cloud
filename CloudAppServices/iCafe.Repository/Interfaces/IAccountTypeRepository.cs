168685e1bd7a7d2f384984bba7f89364ff0f211a
ravikumar-rayani <ravikumar.rayani@gmail.com>
2016-09-26 21:53:44 +0530

    Missing Files
    Missing Files

---
+++ CloudAppServices/iCafe.Repository/Interfaces/IAccountTypeRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iCafe.Entity;
+using iCafe.Data.Infrastructure;
+
+namespace iCafe.Repository.Interfaces
+{
+    public interface IAccountTypeRepository : IRepository<AccountType, int>
+    {
+    }
+}
