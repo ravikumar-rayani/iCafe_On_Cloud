f0697a28b8bc9fd7007a5a2fbab3a7d35a674c83
ravikumar-rayani <ravikumar.rayani@gmail.com>
2016-12-07 00:04:04 +0530

    Added Nested orders datatable

--- CloudAppServices/iCafe.Web/Areas/App/Controllers/OrdersController.cs
+++ CloudAppServices/iCafe.Web/Areas/App/Controllers/OrdersController.cs
@@ -14,7 +14,62 @@ namespace iCafe.Web.Areas.App.Controllers
         // GET: /Orders/
         public ActionResult Index()
         {
-            return View();
+            List<OrderVM> orders = new List<OrderVM>() { 
+                new OrderVM(){ OrderId = 1234, Customer = "Customer", UserName = "User", TotalPrice = 1000.00m, PaymentStatus = "paid", 
+                             PaymentType="cashless", RatingOnFood=4,RatingOnService=3, Feedback="not bad", CreatedOn= DateTime.Now,
+                             SubOrders= new List<SubOrder>(){
+                                        new SubOrder(){ SubOrderId=234, SubTotalPrice=500.00m, OrderStatus="Delivered", CreatedOn=DateTime.Now.Date,
+                                                        SubOrderDetails= new List<SubOrderDetail>(){ 
+                                                                         new SubOrderDetail(){ ItemName = "Soup", Quantity=1, ItemType="regular", AppliedPrice=100.00m},
+                                                                         new SubOrderDetail(){ ItemName = "Starter", Quantity=1, ItemType="regular", AppliedPrice=150.00m},
+                                                                         new SubOrderDetail(){ ItemName = "Biryani", Quantity=1, ItemType="regular", AppliedPrice=250.00m},
+                                                        }
+                                        },
+                                        new SubOrder(){ SubOrderId=234, SubTotalPrice=500.00m, OrderStatus="Delivered", CreatedOn=DateTime.Now.Date,
+                                                        SubOrderDetails= new List<SubOrderDetail>(){ 
+                                                                         new SubOrderDetail(){ ItemName = "Soup", Quantity=1, ItemType="regular", AppliedPrice=100.00m},
+                                                                         new SubOrderDetail(){ ItemName = "Starter", Quantity=1, ItemType="regular", AppliedPrice=150.00m},
+                                                                         new SubOrderDetail(){ ItemName = "Biryani", Quantity=1, ItemType="regular", AppliedPrice=250.00m},
+                                                        }
+                                        },
+                                        new SubOrder(){ SubOrderId=234, SubTotalPrice=500.00m, OrderStatus="Delivered", CreatedOn=DateTime.Now,
+                                                        SubOrderDetails= new List<SubOrderDetail>(){ 
+                                                                         new SubOrderDetail(){ ItemName = "Soup", Quantity=1, ItemType="regular", AppliedPrice=100.00m},
+                                                                         new SubOrderDetail(){ ItemName = "Starter", Quantity=1, ItemType="regular", AppliedPrice=150.00m},
+                                                                         new SubOrderDetail(){ ItemName = "Biryani", Quantity=1, ItemType="regular", AppliedPrice=250.00m},
+                                                        }
+                                        },
+                             }
+                },
+                new OrderVM(){ OrderId = 2234, Customer = "Customer", UserName = "User", TotalPrice = 1000.00m, PaymentStatus = "paid", 
+                             PaymentType="cashless", RatingOnFood=4,RatingOnService=3, Feedback="not bad", CreatedOn= DateTime.Now,
+                             SubOrders= new List<SubOrder>(){
+                                        new SubOrder(){ SubOrderId=234, SubTotalPrice=500.00m, OrderStatus="Delivered", CreatedOn=DateTime.Now,
+                                                        SubOrderDetails= new List<SubOrderDetail>(){ 
+                                                                         new SubOrderDetail(){ ItemName = "Soup", Quantity=1, ItemType="regular", AppliedPrice=100.00m},
+                                                                         new SubOrderDetail(){ ItemName = "Starter", Quantity=1, ItemType="regular", AppliedPrice=150.00m},
+                                                                         new SubOrderDetail(){ ItemName = "Biryani", Quantity=1, ItemType="regular", AppliedPrice=250.00m},
+                                                        }
+                                        },
+                                        new SubOrder(){ SubOrderId=234, SubTotalPrice=500.00m, OrderStatus="Delivered", CreatedOn=DateTime.Now,
+                                                        SubOrderDetails= new List<SubOrderDetail>(){ 
+                                                                         new SubOrderDetail(){ ItemName = "Soup", Quantity=1, ItemType="regular", AppliedPrice=100.00m},
+                                                                         new SubOrderDetail(){ ItemName = "Starter", Quantity=1, ItemType="regular", AppliedPrice=150.00m},
+                                                                         new SubOrderDetail(){ ItemName = "Biryani", Quantity=1, ItemType="regular", AppliedPrice=250.00m},
+                                                        }
+                                        },
+                                        new SubOrder(){ SubOrderId=234, SubTotalPrice=500.00m, OrderStatus="Delivered", CreatedOn=DateTime.Now,
+                                                        SubOrderDetails= new List<SubOrderDetail>(){ 
+                                                                         new SubOrderDetail(){ ItemName = "Soup", Quantity=1, ItemType="regular", AppliedPrice=100.00m},
+                                                                         new SubOrderDetail(){ ItemName = "Starter", Quantity=1, ItemType="regular", AppliedPrice=150.00m},
+                                                                         new SubOrderDetail(){ ItemName = "Biryani", Quantity=1, ItemType="regular", AppliedPrice=250.00m},
+                                                        }
+                                        },
+                             }
+                },
+            };
+
+            return View(orders);
         }
 
         public ActionResult Orders()
@@ -76,5 +131,5 @@ namespace iCafe.Web.Areas.App.Controllers
 
             return View(orders);
         }
-	}
+    }
 }
